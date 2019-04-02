90aa93aa8031e4c81e09b0f2eb9d0b691717c7b7
nereuslch <chihang_lch@126.com>
2019-04-02 22:47:12 +0800

    graph

---
+++ KeysandRooms.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func canVisitAllRooms(rooms [][]int) bool {
+	visited := make([]bool,len(rooms))
+	dfs(rooms,rooms[0],visited)
+	fmt.Println(visited)
+	for _,data := range visited {
+		if !data {
+			return false
+		}
+	}
+
+	return true
+}
+
+func dfs(room [][]int,rec []int,visited []bool) {
+	for _, idx := range rec {
+		if visited[idx] {
+			continue
+		}
+		visited[idx] =true
+		dfs(room,room[idx],visited)
+	}
+}
+
+func main() {
+	canVisitAllRooms([][]int{{1},{2},{3},{}})
+}
