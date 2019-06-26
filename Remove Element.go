bba35d22033569c157fe2d265760441dc53e9637
nereuslch <chihang_lch@126.com>
2019-06-26 21:46:44 +0800

    add

---
+++ Remove Element.go
@@ -0,0 +1,14 @@
+package Leetcode
+
+func removeElement(nums []int, val int) int {
+	i, j := 0, len(nums)-1
+	for ; i <= j; {
+		if nums[i] == val {
+			nums[i] = nums[j]
+			j --
+		} else {
+			i ++
+		}
+	}
+	return i
+}
